ac693169b5d7d130c2484cfce5f70d39c30e2617
Danielium <Lapshin_danila73@mail.ru>
2026-06-20 15:45:41 +0300

    Remove forced reload hack

--- src/components/TelegramInit.tsx
+++ src/components/TelegramInit.tsx
@@ -19,17 +19,6 @@ export function TelegramInit() {
       
       tryFullscreen();
       setTimeout(tryFullscreen, 100);
-      setTimeout(tryFullscreen, 500);
-      setTimeout(tryFullscreen, 1000);
-      setTimeout(tryFullscreen, 2000);
-
-      // Жесткий хак: если это первый запуск сессии, принудительно перезагружаем страницу.
-      // Telegram iOS/Android багует с requestFullscreen при первом открытии шторки.
-      // При перезагрузке шторка уже открыта, и фуллскрин срабатывает мгновенно.
-      if (!sessionStorage.getItem("tg_reloaded_for_fullscreen")) {
-        sessionStorage.setItem("tg_reloaded_for_fullscreen", "true");
-        window.location.reload();
-      }
       
       tg.setHeaderColor("#000000");
       tg.setBackgroundColor("#000000");
