d23cdcc224a9530ac466f32924b579e3c5954082
Danielium <Lapshin_danila73@mail.ru>
2026-07-12 18:44:15 +0400

    style: add italic to club distance to match athletes tab

--- src/components/GlobalClubs.tsx
+++ src/components/GlobalClubs.tsx
@@ -163,7 +163,7 @@ export default function GlobalClubs({ inClub }: { inClub?: boolean }) {
                 </div>
               </div>
               <div className="text-right flex flex-col items-end justify-center">
-                <p className="text-2xl font-black tracking-tighter text-white/90 drop-shadow-md">{club.totalClubDistance.toFixed(1)}</p>
+                <p className="text-2xl font-black italic tracking-tighter text-white/90 drop-shadow-md">{club.totalClubDistance.toFixed(1)}</p>
                 <p className="text-[9px] text-primary uppercase font-black tracking-widest mt-0.5">КМ</p>
               </div>
             </div>
